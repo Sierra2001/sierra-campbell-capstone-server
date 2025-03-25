6fd65c9eb05abde76f08bc074254a8fb690c9a5c
sierra2001 <sierracamp09@gmail.com>
2025-03-25 09:04:14 -0400

    final push

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import workoutRoutes from "./routes/workoutRoutes.js";
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.use("/workouts", workoutRoutes);
 
--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -1,5 +1,6 @@
 import express, { Router } from "express";
 import fs from "fs";
+import { v4 as uuid } from "uuid";
 //the type
 //by the id
 //post
@@ -9,7 +10,7 @@ const router = express.Router();
 router.route("/:id").get((req, res) => {
   const { id } = req.params;
   const workouts = JSON.parse(fs.readFileSync("./data/workouts.json"));
-  const selectedWorkout = workouts.filter(
+  const selectedWorkout = workouts.find(
     (workout) => workout.id === parseInt(id)
   );
 
@@ -18,6 +19,37 @@ router.route("/:id").get((req, res) => {
   }
 
   res.json(selectedWorkout);
+});
+
+router.route("/:id/comments").post((req, res) => {
+  const { id } = req.params;
+  const { name, comment } = req.body;
+  if (!name || !comment) {
+    return res
+      .status(404)
+      .json({ message: "Name and comment must be provided" });
+  }
+
+  const workouts = JSON.parse(fs.readFileSync("./data/workouts.json"));
+  const selectedWorkout = workouts.find(
+    (workout) => workout.id === parseInt(id)
+  );
+
+  if (selectedWorkout.length === 0) {
+    return res.status(404).json({ message: "No workout found" });
+  }
+
+  const newComment = {
+    id: uuid(),
+    timestamp: Date.now(),
+    name: name,
+    comment: comment,
+  };
+
+  selectedWorkout.comments.push(newComment);
+  fs.writeFileSync("./data/workouts.json", JSON.stringify(workouts));
+
+  res.status(201).json(selectedWorkout);
 });
 
 router.route("/type/:type").get((req, res) => {
